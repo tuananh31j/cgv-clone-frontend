cdcf75a4aea3d09af736cb8ab7871e8836ff2b73
Nguyen Tuan Anh <tuananh31j@gmail.com>
2024-03-31 21:46:31 +0700

    fix: used store to reload

--- src/components/AuthPathControl/AuthPathControl.tsx
+++ src/components/AuthPathControl/AuthPathControl.tsx
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { RootState } from '~/store/store';
 import { getCookie } from '~/utilities/cookie';
 
 const AuthPathControl = ({ children }: { children: React.ReactNode }) => {
+    useSelector((state: RootState) => state.auth.login.currentUser.id);
     const accessToken = localStorage.getItem('accessToken');
     const refreshToken = getCookie('refreshToken');
-    if (!accessToken && refreshToken) return <Navigate to={'/login'} />;
+    if (!accessToken && !refreshToken) return <Navigate to={'/login'} />;
 
     return children;
 };
--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { RootState } from '~/store/store';
 import { getCookie } from '~/utilities/cookie';
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
     const accessToken = localStorage.getItem('accessToken');
     const refreshToken = getCookie('refreshToken');
+    const token = useSelector((state: RootState) => state.auth.login.currentUser.id);
+
     if (accessToken && refreshToken) {
         return <Navigate to='/not-found' replace />;
     }
